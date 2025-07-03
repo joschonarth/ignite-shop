47533c4e09b0f834bbbe2fe13fcb4044448fecce
joschonarth <joschonarth@gmail.com>
2025-07-02 22:06:07 -0300

    feat: create product page layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,10 +30,9 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map((product) => {
         return (
-          <Link href={`/product/${product.id}`} key={product.id}>
+          <Link href={`/product/${product.id}`} key={product.id} passHref>
             <Product className="keen-slider__slide">
               <Image src={product.imageUrl} width={520} height={480} alt="" />
-
               <footer>
                 <strong>{product.name}</strong>
                 <span>{product.price}</span>
