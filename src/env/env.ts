8d08dd2b0e1a139b81582117d62642a08e3b41b4
joschonarth <joschonarth@gmail.com>
2025-07-03 22:38:28 -0300

    feat: add Stripe checkout

--- src/env/env.ts
+++ src/env/env.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
+  NEXT_URL: z.string(),
   STRIPE_PUBLIC_KEY: z.string().min(1, 'A STRIPE_PUBLIC_KEY é obrigatória'),
   STRIPE_SECRET_KEY: z.string().min(1, 'A STRIPE_SECRET_KEY é obrigatória'),
 })
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -16,10 +16,15 @@ interface ProductProps {
     imageUrl: string
     price: string
     description: string
+    defaultPriceId: string
   }
 }
 
 export default function Product({ product }: ProductProps) {
+  function handleBuyButton() {
+    console.log(product.defaultPriceId)
+  }
+
   return (
     <ProductContainer>
       <ImageContainer>
@@ -32,7 +37,7 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
 
-        <button>Comprar agora</button>
+        <button onClick={handleBuyButton}>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   )
@@ -79,6 +84,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
           currency: 'BRL',
         }).format(price.unit_amount / 100),
         description: product.description,
+        defaultPriceId: price.id,
       },
     },
     revalidate: 60 * 60 * 1, // 1 hours
